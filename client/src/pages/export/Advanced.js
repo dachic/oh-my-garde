0bcee9b3a28ce8571246d7ca35736ddc23d6e0b6
Dylan Settbon <dylans_set@hotmail.fr>
2020-04-01 16:12:42 +0200

    [FIX] Update function

---
+++ client/src/pages/export/Advanced.js
@@ -0,0 +1,347 @@
+import React from 'react';
+import { Row, Col, Card, CardBody, Input, UncontrolledDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider, { Search, CSVExport } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+
+import PageTitle from '../../components/PageTitle';
+
+const records = [
+       {
+        "firstname": "Philippine",
+        "lastname": "Baudry",
+        "horaire": "6h-8h",
+        "name": "Pharmacie des internes",
+        "nbJours": 1
+        },
+        {
+        "firstname": "Dylan",
+        "lastname": "Settbon",
+        "horaire": "8h-20h",
+        "name": "Pharmacie des internes",
+        "nbJours": 1
+        }
+];
+
+const columns = [
+    {
+        dataField: 'firstname',
+        text: 'firstname',
+        sort: true,
+    },
+    {
+        dataField: 'lastname',
+        text: 'lastname',
+        sort: false,
+    },
+    {
+        dataField: 'horaire',
+        text: 'Horaire',
+        sort: true,
+    },
+    {
+        dataField: 'name',
+        text: 'Hopital',
+        sort: false,
+    },
+    {
+        dataField: 'nbJours',
+        text: 'Nombre de jour',
+        sort: true,
+    },
+];
+
+const defaultSorted = [
+    {
+        dataField: 'id',
+        order: 'asc',
+    },
+];
+
+const sizePerPageRenderer = ({ options, currSizePerPage, onSizePerPageChange }) => (
+    <React.Fragment>
+        <label className="d-inline mr-1">Show</label>
+        <Input type="select" name="select" id="no-entries" className="custom-select custom-select-sm d-inline col-1"
+            defaultValue={currSizePerPage}
+            onChange={(e) => onSizePerPageChange(e.target.value)}>
+            {options.map((option, idx) => {
+                return <option key={idx}>{option.text}</option>
+            })}
+        </Input>
+        <label className="d-inline ml-1">entries</label>
+    </React.Fragment>
+);
+
+const TableWithPaginationAndSort = () => {
+    const customTotal = (from, to, size) => (
+        <span className="react-bootstrap-table-pagination-total ml-4">
+            Showing {from} to {to} of {size} Results
+        </span>
+    );
+
+    const paginationOptions = {
+        paginationSize: 5,
+        pageStartIndex: 1,
+        firstPageText: 'First',
+        prePageText: 'Back',
+        nextPageText: 'Next',
+        lastPageText: 'Last',
+        nextPageTitle: 'First page',
+        prePageTitle: 'Pre page',
+        firstPageTitle: 'Next page',
+        lastPageTitle: 'Last page',
+        showTotal: true,
+        paginationTotalRenderer: customTotal,
+        sizePerPageRenderer: sizePerPageRenderer,
+        sizePerPageList: [
+            {
+                text: '5',
+                value: 5,
+            },
+            {
+                text: '10',
+                value: 10,
+            },
+            {
+                text: '25',
+                value: 25,
+            },
+            {
+                text: 'All',
+                value: records.length,
+            },
+        ], // A numeric array is also available. the purpose of above example is custom the text
+    };
+
+    return (
+        <Card>
+            <CardBody>
+                <h4 className="header-title mt-0 mb-1">Pagination &amp; Sort</h4>
+                <p className="sub-header">A simple example of table with pagination and column sorting</p>
+
+                <BootstrapTable
+                    bootstrap4
+                    keyField="id"
+                    data={records}
+                    columns={columns}
+                    defaultSorted={defaultSorted}
+                    pagination={paginationFactory(paginationOptions)}
+                    wrapperClasses="table-responsive"
+                />
+            </CardBody>
+        </Card>
+    );
+};
+
+const TableWithSearch = () => {
+    const { SearchBar } = Search;
+    const { ExportCSVButton } = CSVExport;
+
+    return (
+        <Card>
+            <CardBody>
+                <h4 className="header-title mt-0 mb-1">Search and Export</h4>
+                <p className="sub-header">A Table allowing search and export the data in CSV format</p>
+
+                <ToolkitProvider
+                    bootstrap4
+                    keyField="id"
+                    data={records}
+                    columns={columns}
+                    search
+                    exportCSV={{ onlyExportFiltered: true, exportAll: false }}>
+                    {props => (
+                        <React.Fragment>
+                            <Row>
+                                <Col>
+                                    <SearchBar {...props.searchProps} />
+                                </Col>
+                                <Col className="text-right">
+                                    <ExportCSVButton {...props.csvProps} className="btn btn-primary">
+                                        Export CSV
+                                    </ExportCSVButton>
+                                </Col>
+                            </Row>
+
+                            <BootstrapTable
+                                {...props.baseProps}
+                                bordered={false}
+                                defaultSorted={defaultSorted}
+                                pagination={paginationFactory({ sizePerPage: 5, sizePerPageRenderer: sizePerPageRenderer, sizePerPageList: [{ text: '5', value: 5, }, { text: '10', value: 10 }, { text: '25', value: 25 }, { text: 'All', value: records.length }] })}
+                                wrapperClasses="table-responsive"
+                            />
+                        </React.Fragment>
+                    )}
+                </ToolkitProvider>
+            </CardBody>
+        </Card>
+    );
+};
+
+const TableWithSeletableRows = () => {
+    const selectRow = {
+        mode: 'checkbox',
+        clickToSelect: true,
+        style: { backgroundColor: '#727cf5', color: '#fff' },
+    };
+
+    return (
+        <Card>
+            <CardBody>
+                <h4 className="header-title mt-0 mb-1">Multiple Row Selection</h4>
+                <p className="sub-header">This table allowing selection of multiple rows</p>
+
+                <BootstrapTable
+                    bootstrap4
+                    keyField="id"
+                    bordered={false}
+                    data={records}
+                    columns={columns}
+                    pagination={paginationFactory({ sizePerPage: 5, sizePerPageRenderer: sizePerPageRenderer, sizePerPageList: [{ text: '5', value: 5, }, { text: '10', value: 10 }, { text: '25', value: 25 }, { text: 'All', value: records.length }] })}
+                    selectRow={selectRow}
+                    wrapperClasses="table-responsive"
+                />
+            </CardBody>
+        </Card>
+    );
+};
+
+const TableWithRowExpand = () => {
+    const expandRow = {
+        renderer: row => (
+            <div>
+                <p className="mt-2">{`Hello ${row.name}`}</p>
+                <p>You can render anything here, also you can add additional data on every row object</p>
+                <p>expandRow.renderer callback will pass the origin row object to you</p>
+            </div>
+        ),
+        showExpandColumn: true,
+        onlyOneExpanding: true,
+        expandHeaderColumnRenderer: ({ isAnyExpands }) => {
+            return isAnyExpands ? <i className='uil uil-minus'></i> : <i className='uil uil-plus'></i>;
+        },
+        expandColumnRenderer: ({ expanded }) => {
+            return expanded ? <i className='uil uil-minus'></i> : <i className='uil uil-plus'></i>;
+        },
+    };
+
+    return (
+        <Card>
+            <CardBody>
+                <h4 className="header-title mt-0 mb-1">Expand Row</h4>
+                <p className="sub-header">Expand row to see more additional details</p>
+
+                <BootstrapTable
+                    bootstrap4
+                    keyField="id"
+                    bordered={false}
+                    data={records}
+                    columns={columns}
+                    pagination={paginationFactory({ sizePerPage: 5, sizePerPageRenderer: sizePerPageRenderer, sizePerPageList: [{ text: '5', value: 5, }, { text: '10', value: 10 }, { text: '25', value: 25 }, { text: 'All', value: records.length }] })}
+                    expandRow={expandRow}
+                    wrapperClasses="table-responsive"
+                />
+            </CardBody>
+        </Card>
+    );
+};
+
+const CustomToggleList = ({
+    columns,
+    onColumnToggle,
+    toggles
+}) => (
+        <UncontrolledDropdown className="mb-3">
+            <DropdownToggle tag="button" className="btn btn-white">Select Columns <i className='uil uil-angle-down font-size-15 ml-1 align-middle'></i></DropdownToggle>
+            <DropdownMenu>
+                {columns.map(column => ({
+                    ...column,
+                    toggle: toggles[column.dataField]
+                }))
+                    .map(column => (
+                        <DropdownItem key={column.dataField} onClick={() => onColumnToggle(column.dataField)}>
+                            {column.toggle && <i className='uil uil-check'></i>}
+                            <span className="ml-2">{column.text}</span>
+                        </DropdownItem>
+                    ))
+                }
+            </DropdownMenu>
+        </UncontrolledDropdown>
+    );
+
+const TableWithColumnToggle = () => {
+
+    return (
+        <Card>
+            <CardBody>
+                <h4 className="header-title mt-0 mb-1">Toggle Columns</h4>
+                <p className="sub-header">Show/Hide any column you want</p>
+
+                <ToolkitProvider keyField="id" data={records} columns={columns} columnToggle>
+                    {props => (
+                        <div>
+                            <CustomToggleList {...props.columnToggleProps} />
+                            <BootstrapTable
+                                {...props.baseProps}
+                                bordered={false}
+                                pagination={paginationFactory({ sizePerPage: 5, sizePerPageRenderer: sizePerPageRenderer, sizePerPageList: [{ text: '5', value: 5, }, { text: '10', value: 10 }, { text: '25', value: 25 }, { text: 'All', value: records.length }] })}
+                                wrapperClasses="table-responsive"
+                            />
+                        </div>
+                    )}
+                </ToolkitProvider>
+            </CardBody>
+        </Card>
+    );
+};
+
+const Tables = () => {
+    return (
+        <React.Fragment>
+            <Row className="page-title">
+                <Col md={12}>
+                    <PageTitle
+                        breadCrumbItems={[
+                            { label: 'Tables', path: '/tables/advanced' },
+                            { label: 'Advanced Tables', path: '/tables/advanced', active: true },
+                        ]}
+                        title={'Advanced Tables'}
+                    />
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <TableWithPaginationAndSort />
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <TableWithSearch />
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <TableWithSeletableRows />
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <TableWithRowExpand />
+                </Col>
+            </Row>
+
+            <Row>
+                <Col>
+                    <TableWithColumnToggle />
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
+};
+
+export default Tables;
--- server/src/Controller/UserController.php
+++ server/src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Guard;
+use App\Entity\Pharmacy;
 use App\Entity\DisponibilityHour;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -28,6 +29,7 @@ class UserController extends AbstractController
         $repository_user = $this->getDoctrine()->getRepository(User::class);
         $repository_disponibility_hour = $this->getDoctrine()->getRepository(DisponibilityHour::class);
         $repository_guard = $this->getDoctrine()->getRepository(Guard::class);
+        $repository_pharmacy = $this->getDoctrine()->getRepository(Pharmacy::class);
 
         $array = [];
 
@@ -40,32 +42,43 @@ class UserController extends AbstractController
         // on recupere toutes les gardes
         $guards = $repository_guard->findBy(['status' => 'accepted']);
 
+        // on recupere toutes les pharmacies
+        $pharmacies = $repository_pharmacy->findAll();
+
+        
+
         // on fait une boucle pour chaque interne
         foreach($ids as $id)
         {         
             foreach($hours as $hour)
             {
-                $count = 0;
-                foreach($guards as $guard)
+               
+              foreach($pharmacies as $pharmacy)
                 {
-                    if(($guard->getHour()->getId() == $hour->getId()) && ($guard->getUser()->getId() == $id->getId()))
+                    $count =  0; 
+                    foreach($guards as $guard)
                     {
-                        $count++;
-                        $day = [
-                            'LastName'  => $guard->getUser()->getLastName(),
-                            'FirstName' => $guard->getUser()->getFirstName(),
-                            'Name'      => $guard->getPharmacy()->getName(),
-                            'Horaire'   => $hour->getName(),
-                            'NbGarde'   => $count
-                        ];
+                        if(($guard->getHour()->getId() == $hour->getId()) && ($guard->getUser()->getId() == $id->getId()) && ($guard->getPharmacy()->getId() == $pharmacy->getId()))
+                        {
+                            $count++;
+                            $day = [
+                                'LastName'  => $guard->getUser()->getLastName(),
+                                'FirstName' => $guard->getUser()->getFirstName(),
+                                'Name'      => $guard->getPharmacy()->getName(),
+                                'Horaire'   => $hour->getName(),
+                                'NbGarde'   => $count
+                            ];
+                        }
+                        
+                    }
+                    if(isset($day))
+                    {
+                      array_push($array, $day);
+                      unset($day);
                     }
                 }
-                if(isset($day))
-                {
-                    array_push($array, $day);
-                    unset($day);
-                } 
-            }
+                
+            } 
         }
 
         // unset tout les tableaux
