f3dde05f742cd97105e11902b4dbab1586689b1e
Kaba CONDE <kabaconde15@gmail.com>
2020-04-08 19:18:35 +0200

    Add new script for server build deployment

---
+++ script/rsync_deploy_build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo -e "==> Installing composer dependencies"
+make cinstall-prod
+
+echo -e "==> Update JWT private/public key"
+make jwt
+
+echo -e "==> Compiling emails"
+make compile
+
+echo -e "==> Clearing cache for production"
+make cache-clear
+
+echo -e "==> Deployment done ✅"
+exit 0
